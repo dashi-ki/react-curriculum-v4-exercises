7839041b85d086c57b0015b2a9dedf71793398f4
Daria T <dtolkachova@protonmail.com>
2026-04-10 20:17:08 -0400

    Add Snack components and wire into studentWork
    Introduce SnackHeader, SnackList, and SnackFooter components for the Lesson-02 exercise and add a screenshot asset. studentWork.jsx updated to import and render the new components; SnackList defines a snacks array and uses toSorted to order items by rank before mapping them into an unordered list.

---
+++ src/exercises/lesson-02/SnackFooter.jsx
@@ -0,0 +1,7 @@
+export default function SnackFooter() {
+  return (
+    <div>
+      <p>PS: If you see me eating any of these, take them away immediately.</p>
+    </div>
+  );
+}
--- src/exercises/lesson-02/studentWork.jsx
+++ src/exercises/lesson-02/studentWork.jsx
@@ -1,12 +1,21 @@
 //Lesson-02 Building with ReactDOM and components
 //Exercise: Build a "Snack Ranking App" Component in this file
 //Import components here
+import './SnackHeader.jsx';
+import SnackHeader from './SnackHeader.jsx';
+
+import './SnackList.jsx';
+import SnackList from './SnackList.jsx';
+
+import './SnackFooter.jsx';
+import SnackFooter from './SnackFooter.jsx';
 
 export default function StudentWork() {
   return (
     <div>
-      {/* add JSX here */}
-      <p> Student output will go here</p>
+      <SnackHeader />
+      <SnackList />
+      <SnackFooter />
     </div>
   );
 }
